8a023e37ef521d140ad445dad8ed4a3dfba5ac0a
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-07-16 16:27:59 +1200

    added a simple layout for text input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex flex-col w-screen h-screen items-center justify-between">
+      <h1 className="text-3xl">Text input</h1>
+      <input className="w-[80%] mx-auto my-3" type="text" />
     </div>
   );
 }
